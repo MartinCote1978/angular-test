d873810f782ffee327ece22262f2ebaa530d1403
Martin Cote <martin.cote@gmail.com>
2015-09-14 18:54:12 -0400

    Everything working
    Everything working (except the unit and e2e test stuff)…

--- apps/Apps/common/shared/app/data/detail.controller.js
+++ apps/Apps/common/shared/app/data/detail.controller.js
@@ -4,13 +4,12 @@
 
     angular
         .module('Apps.data')
-        .controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$http',
-                     function($scope, $routeParams, $http) {
+        .controller('PhoneDetailCtrl', ['$scope', '$http', '$stateParams',
+                     function($scope, $http, $stateParams) {
 
-                         // console.log('"$routeParams:"' + $routeParams);
                          var domain = 'https://raw.githubusercontent.com';
                          var url = '/angular/angular-phonecat/master/app/phones/';
-                         $http.get(domain + url + $routeParams.phoneId + '.json')
+                         $http.get(domain + url + $stateParams.phoneId + '.json')
                              .success(function(data) {
                                  $scope.phone = data;
                              });
